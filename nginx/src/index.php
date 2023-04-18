149848412e501380978c8a197b47a82f8234fe4d
Eldar <eldar@Eldars-MacBook-Air.local>
2023-04-18 16:50:17 +0400

    Added more functions

--- nginx/src/index.php
+++ nginx/src/index.php
@@ -1,5 +1,5 @@
 <?php
-function add($num1, $num2) {
+function add1($num1, $num2) {
     while ($num2 != 0) {
       $carry = $num1 & $num2; // calculate the carry
       $num1 = $num1 ^ $num2; // calculate the sum without carry
@@ -7,13 +7,35 @@ function add($num1, $num2) {
     }
     return $num1;
   }
+
+  function add2($num1, $num2) {
+    if ($num2 == 0) {
+      return $num1;
+    } else {
+      $sum = $num1 ^ $num2; // calculate the sum without carry
+      $carry = ($num1 & $num2) << 1; // calculate the carry and shift it to the left
+      return add2($sum, $carry); // recursively add the sum and the carry
+    }
+  }
+  
+  function add3($num1, $num2) {
+    if ($num2 == 0) {
+      return $num1;
+    } else {
+      $sum = $num1 ^ $num2; // calculate the sum without carry
+      $carry = ($num1 & $num2) << 1; // calculate the carry and shift it to the left
+      return add3($sum, $carry); // recursively add the sum and the carry
+    }
+  }
   
 
 
 if (isset($_GET['a']) && isset($_GET['b'])) {
     $a = intval($_GET['a']);
     $b = intval($_GET['b']);
-    $result = add($a, $b);
+    $functions = array('add1', 'add2', 'add3');
+    $selectedFunction = $functions[array_rand($functions)];
+    $result = $selectedFunction($a, $b);
     echo $result;
 } else {
 
--- node/server.js
+++ node/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 
+
 function add(num1, num2) {
   if (num2 === 0) {
     return num1;
@@ -12,10 +13,51 @@ function add(num1, num2) {
   }
 }
 
+
+function add2(num1, num2) {
+  let sum = num1;
+  let carry = num2;
+
+  while (carry !== 0) {
+    let tempSum = sum ^ carry; // calculate the sum without carry
+    carry = (sum & carry) << 1; // calculate the carry and shift it to the left
+    sum = tempSum; // update the sum
+  }
+
+  return sum;
+}
+
+function add3(num1, num2) {
+  let sum, carry;
+
+  do {
+    sum = num1 ^ num2; // calculate the sum without carry
+    carry = (num1 & num2) << 1; // calculate the carry and shift it to the left
+    num1 = sum; // update the sum
+    num2 = carry; // update the carry
+  } while (carry !== 0);
+
+  return sum;
+}
+
+function randomAddition(num1, num2) {
+  const randomNumber = Math.floor(Math.random() * 3); // generate a random number between 0 and 2
+
+  switch (randomNumber) {
+    case 0:
+      return add(num1, num2); // use the first implementation
+    case 1:
+      return add2(num1, num2); // use the second implementation
+    case 2:
+      return add3(num1, num2); // use the third implementation
+  }
+}
+
+
 app.get('/', (req, res) => {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
-  const result = add(a,b);
+  const result = randomAddition(a,b);
   res.status(200).send(result.toString());
 });
 
